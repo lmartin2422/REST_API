52e62ea7ddd4cdb7e8ecc1c2af9a5a84aff40d50
lmartin2422 <lmartin2422@gmail.com>
2023-10-07 17:33:41 -0400

    added mock json data

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,18 @@ import express from 'express'
 
 const router = express.Router();  // initializes the router
 
-// all routes in here are starting with /users because of app.use('/users', userRoutes) in index.js
+const users = [  // mock data for API use
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25  
+      }
+]
+
+// all routes here start with /users because of app.use('/users', userRoutes) in index.js
 router.get('/', (req, res) => {  
+    console.log(users);
     res.send('Hello');
 });
 
-export default router;  // allows us to make use of the router in the index.js file
+export default router;  // allows us to make use of router in index.js file
